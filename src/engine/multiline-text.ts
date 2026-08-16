e4c48a8c87991f8f76d60f42d262f5df0703d7c7
Antti Haavikko <antti.haavikko@gmail.com>
2026-08-16 17:21:00 +0300

    Remove unused text entity stuffs

--- src/engine/multiline-text.ts
+++ src/engine/multiline-text.ts
@@ -6,21 +6,21 @@ export class MultilineTextEntity extends TextEntity {
         ctx.save();
         ctx.rotate(this.options?.angle ?? 0);
         const mod = this.options?.scales ? this.ratio : 1;
-        ctx.font =`${this.fontSize * mod}px ${font}`;
+        ctx.font = `${this.fontSize * mod}px ${font}`;
         ctx.textAlign = this.options?.align ?? 'center';
 
         const lineHeight = this.fontSize * 1.25;
         const lines = this.content.split('\n');
         lines.forEach((line, row) => {
-            const offset =  + row * lineHeight - (lines.length - 1) * lineHeight;
-            if (this.options?.shadow) {
-                ctx.fillStyle = '#000';
-                ctx.fillText(line.replace(/\|/g, ''), this.p.x + this.options.shadow, this.p.y + this.options.shadow + offset);
-            }
+            const offset = + row * lineHeight - (lines.length - 1) * lineHeight;
+            // if (this.options?.shadow) {
+            //     ctx.fillStyle = '#000';
+            //     ctx.fillText(line.replace(/\|/g, ''), this.p.x + this.options.shadow, this.p.y + this.options.shadow + offset);
+            // }
             ctx.fillStyle = this.options?.color ?? '#fff';
             ctx.fillText(line, this.p.x, this.p.y + offset);
         });
-        
+
         ctx.restore();
     }
 
--- src/engine/text.ts
+++ src/engine/text.ts
@@ -8,22 +8,22 @@ export class TextEntity extends Particle {
         super(game, x, y, 0, 0, life, velocity);
     }
 
-    public draw(ctx: CanvasRenderingContext2D): void {
-        ctx.save();
-        ctx.rotate(this.options?.angle ?? 0);
-        const mod = this.options?.scales ? this.ratio : 1;
-        ctx.font =`${this.fontSize * mod}px ${font}`;
-        ctx.textAlign = this.options?.align ?? 'center';
+    // public draw(ctx: CanvasRenderingContext2D): void {
+    //     ctx.save();
+    //     ctx.rotate(this.options?.angle ?? 0);
+    //     const mod = this.options?.scales ? this.ratio : 1;
+    //     ctx.font =`${this.fontSize * mod}px ${font}`;
+    //     ctx.textAlign = this.options?.align ?? 'center';
 
-        if (this.options?.shadow) {
-            ctx.fillStyle = '#000';
-            ctx.fillText(this.content, this.p.x + this.options.shadow, this.p.y + this.options.shadow);
-        }
-        ctx.fillStyle = this.options?.color ?? '#fff';
-        ctx.fillText(this.content, this.p.x, this.p.y);
-        
-        ctx.restore();
-    }
+    //     if (this.options?.shadow) {
+    //         ctx.fillStyle = '#000';
+    //         ctx.fillText(this.content, this.p.x + this.options.shadow, this.p.y + this.options.shadow);
+    //     }
+    //     ctx.fillStyle = this.options?.color ?? '#fff';
+    //     ctx.fillText(this.content, this.p.x, this.p.y);
+
+    //     ctx.restore();
+    // }
 
     // public setColor(color: string): void {
     //     this.options.color = color;
@@ -31,13 +31,13 @@ export class TextEntity extends Particle {
 
     public getWidth(ctx: CanvasRenderingContext2D): number {
         const mod = this.options?.scales ? this.ratio : 1;
-        ctx.font =`${this.fontSize * mod}px ${font}`;
+        ctx.font = `${this.fontSize * mod}px ${font}`;
         return Math.max(...this.content.split('\n').map(t => ctx.measureText(t).width));
     }
 
-    public setOptions(opts: TextOptions): void {
-        this.options = { ...this.options, ...opts };
-    }
+    // public setOptions(opts: TextOptions): void {
+    //     this.options = { ...this.options, ...opts };
+    // }
 }
 
 export interface TextOptions {
